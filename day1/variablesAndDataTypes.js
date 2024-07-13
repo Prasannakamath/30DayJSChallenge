1de1b649ffa69e57518b02eb8ed82df61c2c7ee4
Prasanna Kamath <kamathp28@gmail.com>
2024-07-13 16:10:19 +0530

    task2 complete

--- day1/variablesAndDataTypes.js
+++ day1/variablesAndDataTypes.js
@@ -1,3 +1,7 @@
 //Task1: declare a variable using 'var', assign it a number, log its value to console.
 var num = 1;
 console.log(num);
+
+//Task2: declare a variable using 'let', assign it a string, log its value to console
+let str = "Prasanna Kamath";
+console.log(str);
