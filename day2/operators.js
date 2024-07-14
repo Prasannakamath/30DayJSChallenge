1002c8cf39d85c54367fe303b384231e1b053636
Prasanna Kamath <kamathp28@gmail.com>
2024-07-14 16:58:48 +0530

    day2 task2 complete

--- day2/operators.js
+++ day2/operators.js
@@ -4,3 +4,10 @@ function sum(a, b) {
 }
 
 console.log(sum(2, 3));
+
+//task2: write a program to substact two numbers and log the result to console.
+function substract(a, b) {
+  return a - b;
+}
+
+console.log(substract(5, 2));
