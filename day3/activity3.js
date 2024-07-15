5cc7eb70c1c1ec089d43c722af0ea60f021a04b5
Prasanna Kamath <kamathp28@gmail.com>
2024-07-15 19:23:08 +0530

    day3 task5 complete

--- day3/activity3.js
+++ day3/activity3.js
@@ -21,3 +21,28 @@ function findDayOfTheWeek(dayNum) {
 }
 
 console.log(findDayOfTheWeek(4));
+
+//task5: derive grades from A to F based on scores provided and log the result to console.
+function deriveGrade(score) {
+  switch (true) {
+    case score >= 80 && score <= 100:
+      return "A";
+    case score >= 60 && score < 80:
+      return "B";
+    case score >= 40 && score < 60:
+      return "C";
+    case score >= 20 && score < 40:
+      return "D";
+    case score < 20:
+      return "F";
+    default:
+      return "X";
+  }
+}
+let score = 199;
+let grade = deriveGrade(score);
+if (grade !== "X") {
+  console.log(`Grade derived for score of ${score} is ${grade}`);
+} else {
+  console.log(`Grade cannot be derived for the given score of ${score}`);
+}
