cc1af026cab5a19a21d9ec44ec44efe70d4ab152
Prasanna Kamath <kamathp28@gmail.com>
2024-07-15 18:41:31 +0530

    day3 task2 complete

--- day3/activity1.js
+++ day3/activity1.js
@@ -10,3 +10,16 @@ function checkNum(a) {
 }
 
 console.log(checkNum(10));
+
+//task2: check if a person is eligible to vote(age>=18) and log the result to console.
+function isEligibleToVote(age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+let age = 12;
+console.log(
+  `Person with age ${age} is Eligible to Vote? : ${isEligibleToVote(age)}`
+);
