ab9b15dbe70d737efb65f985f7d084507629bb1c
Prasanna Kamath <kamathp28@gmail.com>
2024-07-15 19:26:49 +0530

    day3 task6 complete

---
+++ day3/activity4.js
@@ -0,0 +1,7 @@
+//task6: check if a number is even or odd using ternary operator and log the result to console.
+function isEven(num) {
+  return num % 2 === 0 ? true : false;
+}
+
+let num = 4;
+console.log(`is ${num} Even? : ${isEven(num)}`);
