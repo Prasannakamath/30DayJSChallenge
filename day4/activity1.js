1b30112d7cef08a701f029416230de7478790e5f
Prasanna Kamath <kamathp28@gmail.com>
2024-07-16 19:04:08 +0530

    day4 task2 complete

--- day4/activity1.js
+++ day4/activity1.js
@@ -2,3 +2,8 @@
 for (let i = 1; i <= 10; i++) {
   console.log(i);
 }
+
+//task2: Print Multiplication table of 5 using for loop.
+for (let i = 1; i <= 10; i++) {
+  console.log(`5 x ${i} = ${5 * i}`);
+}
