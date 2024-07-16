8d2ae503682f8f047cdf136b862a7393aebf3c75
Prasanna Kamath <kamathp28@gmail.com>
2024-07-16 19:26:22 +0530

    day4 task9 complete

--- day4/activity5.js
+++ day4/activity5.js
@@ -5,3 +5,11 @@ for (let i = 1; i <= 10; i++) {
   }
   console.log(i);
 }
+
+//task9: print numbers from 1 to 10 but stop the loop at number 7 using 'break'.
+for (let i = 1; i <= 10; i++) {
+  if (i === 7) {
+    break;
+  }
+  console.log(i);
+}
