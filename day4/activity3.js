34a9537513517fe7318e7b266f58b20bb69c13df
Prasanna Kamath <kamathp28@gmail.com>
2024-07-16 19:17:55 +0530

    day4 task6 complete

--- day4/activity3.js
+++ day4/activity3.js
@@ -4,3 +4,22 @@ do {
   console.log(i);
   i++;
 } while (i <= 5);
+
+//task6: calculate factorial of a number using do-while loop.
+function findFactorialOf(num) {
+  let factorial = 1;
+  if (num == 0 || num == 1) {
+    return factorial;
+  } else if (num > 0) {
+    do {
+      factorial *= num;
+      num--;
+    } while (num > 1);
+    return factorial;
+  } else {
+    console.log("Cannot find factorial of negative number");
+    return;
+  }
+}
+
+console.log(findFactorialOf(20));
