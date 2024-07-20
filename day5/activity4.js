67ad9aba39ff290c4b87f991be63bf86658d300f
Prasanna Kamath <kamathp28@gmail.com>
2024-07-20 14:41:40 +0530

    day5 task7 complete

---
+++ day5/activity4.js
@@ -0,0 +1,6 @@
+//task7: Write a function to calculate product of 2 numbers and return the result.
+//Provide some default value to 2nd parameter.
+function multiply(a, b = 2) {
+  return a * b;
+}
+console.log(multiply(3));
