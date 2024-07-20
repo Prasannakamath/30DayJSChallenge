28c87a074f003d4d50d1b8a9324582cce35f5e51
Prasanna Kamath <kamathp28@gmail.com>
2024-07-20 14:18:49 +0530

    day5 task2 complete

--- day5/activity1.js
+++ day5/activity1.js
@@ -7,3 +7,10 @@ function isEven(num) {
 }
 let num = 5;
 console.log(`is ${num} Even? : ${isEven(num)}`);
+
+//task2: write a function to return square of a number and log the result to console.
+function findSquare(num) {
+  return num ** 2;
+}
+let num1 = 12;
+console.log(`Square of ${num1} is: ${findSquare(num1)}`);
