43d21fe2b2adfedde9054866b9d79218bcabe0d8
Prasanna Kamath <kamathp28@gmail.com>
2024-07-20 14:37:12 +0530

    day5 task7 complete

--- day5/activity3.js
+++ day5/activity3.js
@@ -1,3 +1,9 @@
 //task5: write an arrow function to find sum of 2 numbers and log the result to console.
 let sum = (a, b) => a + b;
 console.log(sum(3, 4));
+
+//task6: Write an arrow function to check if a string contains specific character and return the result.
+let Str = "PRASANNA";
+let ch = "X";
+let findChar = (str, ch) => str.includes(ch);
+console.log(findChar(Str, ch));
