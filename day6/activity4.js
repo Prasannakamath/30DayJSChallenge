20cab74adb0311bd485550d8084cfe0bae99b92b
Prasanna Kamath <kamathp28@gmail.com>
2024-07-22 20:40:50 +0530

    day6 task11 complete

--- day6/activity4.js
+++ day6/activity4.js
@@ -3,3 +3,6 @@ let arr = [1, 2, 3, 4, 5];
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
+
+//task11: use ForEach method to iterate over each element in array and log it to console.
+arr.forEach((ele) => console.log(ele));
