c3f7f6dfe1409e7647be4131d922c2b6f580cbcb
Prasanna Kamath <kamathp28@gmail.com>
2024-07-22 20:53:19 +0530

    day6 task12 complete

---
+++ day6/activity5.js
@@ -0,0 +1,19 @@
+//task12: create a 2-dimensional array and log entire array to console.
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(arr);
+function printMatrix(arr) {
+  let matrix = [];
+  for (let i = 0; i < arr.length; i++) {
+    let colObj = {};
+    for (let j = 0; j < arr[i].length; j++) {
+      colObj[j] = arr[i][j];
+    }
+    matrix.push(colObj);
+  }
+  console.table(matrix);
+}
+printMatrix(arr);
