ed2631f86afb8b75e8bfe30ccdcc7381d6ed4c9d
Prasanna Kamath <kamathp28@gmail.com>
2024-07-22 20:19:56 +0530

    day6 task2 complete

--- day6/activity1.js
+++ day6/activity1.js
@@ -1,3 +1,7 @@
 //task1: create an array of numbers 1 to 5 and log the array to console.
 let arr = [1, 2, 3, 4, 5];
 console.log(arr);
+
+//task2: access first and last elements of array and log them to console.
+console.log(`First Element of array is: ${arr[0]}`);
+console.log(`Last element of array is: ${arr[arr.length - 1]}`);
