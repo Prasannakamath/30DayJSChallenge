2e755726418b747f930d30a06cec95baec499122
Prasanna Kamath <kamathp28@gmail.com>
2024-07-22 20:29:29 +0530

    day6 task7 complete

---
+++ day6/activity3.js
@@ -0,0 +1,5 @@
+//task7: use map method to create new array where each number is doubled.
+//log new array to console.
+let arr = [1, 2, 3, 4, 5];
+let newArr = arr.map((ele) => ele * 2);
+console.log(newArr);
