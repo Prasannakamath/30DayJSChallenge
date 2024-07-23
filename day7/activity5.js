69b572858d8d6b128b9869d5a839820effa1cd38
Prasanna Kamath <kamathp28@gmail.com>
2024-07-23 19:40:25 +0530

    day7 task9 complete

--- day7/activity5.js
+++ day7/activity5.js
@@ -10,3 +10,7 @@ for (let property in book) {
     `this book has property ${property} with value ${book[property]}`
   );
 }
+
+//task9: use Object.Keys and Object.Values method to log all keys and values of book object to console.
+console.log(`Book object has these keys: ${Object.keys(book)}`);
+console.log(`These are the values present in book: ${Object.values(book)}`);
