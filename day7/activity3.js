1a53f51cc4e1d148c732703247f3c9f92389717e
Prasanna Kamath <kamathp28@gmail.com>
2024-07-23 19:28:04 +0530

    day7 task5 complete

---
+++ day7/activity3.js
@@ -0,0 +1,20 @@
+//task5: create a nested Object named Library with properties name and books.
+//books is an array of objects consisting of several book details.
+//log the object to console.
+let library = {
+  name: "Central Library",
+  books: [
+    {
+      title: "Catalyst",
+      author: "Prasanna",
+      year: 1998,
+    },
+    {
+      title: "Rich Dad Poor Dad",
+      author: "Robert",
+      year: 1980,
+    },
+  ],
+};
+
+console.log(library);
