e48c73eae84845f4e18a14f2e300b18e506413d3
Prasanna Kamath <kamathp28@gmail.com>
2024-07-23 18:53:23 +0530

    day7 task1 complete

---
+++ day7/activity1.js
@@ -0,0 +1,8 @@
+//task1: create an Object representing properties of a book such as title, author and year.
+//log the object to console.
+let book = {
+  title: "Catalyst",
+  author: "Prasanna",
+  year: 2020,
+};
+console.log(book);
