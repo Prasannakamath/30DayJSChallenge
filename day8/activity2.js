94cab005baa51dc29cbed1c98ca18275f945a429
Prasanna Kamath <kamathp28@gmail.com>
2024-07-24 20:37:53 +0530

    day8 task3 complete

---
+++ day8/activity2.js
@@ -0,0 +1,6 @@
+//task3: use array destructuring to extract first and second element from an array of numbers.
+//log them to console.
+let arr = [1, 2, 3, 4, 5];
+[firstEle, secondEle] = [arr[0], arr[1]];
+console.log(`first Element: ${firstEle}.
+Second Element: ${secondEle}`);
