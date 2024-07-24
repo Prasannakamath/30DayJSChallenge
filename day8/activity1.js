f0a2d9cd1a85e5606ed862f248ddfa6df043b3dd
Prasanna Kamath <kamathp28@gmail.com>
2024-07-24 20:33:32 +0530

    day8 task1 complete

---
+++ day8/activity1.js
@@ -0,0 +1,6 @@
+//task1: using template literals, create a string containing variable of person's name and age.
+//log string to console.
+let name = "Prasanna";
+let age = 27;
+let greet = `Hello ${name}, you are already ${age} old`;
+console.log(greet);
