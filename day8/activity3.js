47d151a93e4af242a67dfa8178421bd7f3fd7172
Prasanna Kamath <kamathp28@gmail.com>
2024-07-24 20:47:27 +0530

    day8 task6 complete

--- day8/activity3.js
+++ day8/activity3.js
@@ -5,3 +5,11 @@ let arr = [1, 2, 3, 4, 5];
 
 let newArr = [...arr, 6, 7];
 console.log(newArr);
+
+//task6: use the rest operator in function to accept an arbitary number of arguments,
+//sum them and return the result.
+function sum(...values) {
+  return values.reduce((acc, it) => acc + it, 0);
+}
+
+console.log(sum(4, 5, 6, 7));
