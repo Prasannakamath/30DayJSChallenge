938f59e5ba3e9aec589c7c7dc8a022632673a35e
Prasanna Kamath <kamathp28@gmail.com>
2024-07-24 20:44:37 +0530

    day8 task5 complete

---
+++ day8/activity3.js
@@ -0,0 +1,7 @@
+//task5: Use SPread operator to create new array that includes all elements of old array.
+//new array must include additional elements too.
+//log new array to console.
+let arr = [1, 2, 3, 4, 5];
+
+let newArr = [...arr, 6, 7];
+console.log(newArr);
