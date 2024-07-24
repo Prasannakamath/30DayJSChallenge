3513a4304cdc9b37fd8d3d26f60e5414ba3938dd
Prasanna Kamath <kamathp28@gmail.com>
2024-07-24 20:41:20 +0530

    day8 task4 complete

--- day8/activity2.js
+++ day8/activity2.js
@@ -4,3 +4,15 @@ let arr = [1, 2, 3, 4, 5];
 [firstEle, secondEle] = [arr[0], arr[1]];
 console.log(`first Element: ${firstEle}.
 Second Element: ${secondEle}`);
+
+//task4: use Object destructuring to extract title and author from Book object.
+//log them to console.
+let book = {
+  title: "Harry Potter",
+  author: "PK",
+  year: 1990,
+};
+
+let { title, author } = book;
+console.log(`title: ${title}.
+Author: ${author}`);
