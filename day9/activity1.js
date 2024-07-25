08c6d4592aa5aa99bf813fd06f78052b36a6ccfa
Prasanna Kamath <kamathp28@gmail.com>
2024-07-25 12:24:22 +0530

    day9 task2 complete

--- day9/activity1.js
+++ day9/activity1.js
@@ -2,3 +2,13 @@
 let pTag = document.getElementById("id1");
 console.log(pTag.innerHTML);
 pTag.innerHTML = "DOM Manipulated";
+
+//task2: Select Html element by classname and change its background color.
+let mainClassElements = document.getElementsByClassName("main");
+if (mainClassElements.length === 1) {
+  mainClassElements[0].style.backgroundColor = "blue";
+} else {
+  mainClassElements.array.forEach((element) => {
+    element.style.backgroundColor = "blue";
+  });
+}
