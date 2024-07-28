153ccd6dce9717f9f151e8256ad1ba0eeb80bee5
Prasanna Kamath <kamathp28@gmail.com>
2024-07-28 10:44:32 +0530

    day11 task8 complete

---
+++ day11/activity5.js
@@ -0,0 +1,31 @@
+//task8: use 'promise.all' to wait for multiple promises to resolve and log there result.
+function promise1(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("promise1 resolved");
+    }, time);
+  });
+}
+
+function promise2(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("promise2 resolved");
+    }, time + 1000);
+  });
+}
+
+function promise3(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("promise3 resolved");
+    }, time + 2000);
+  });
+}
+
+let time = 2000;
+Promise.all([promise1(time), promise2(time), promise3(time)]).then(
+  (returnArray) => {
+    console.log(returnArray);
+  }
+);
