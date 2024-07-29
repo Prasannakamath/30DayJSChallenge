43362a9a32a77819e2b3aa80dacdab41a4d69821
Prasanna Kamath <kamathp28@gmail.com>
2024-07-29 12:10:45 +0530

    day12 task2 complete

--- day12/activity1.js
+++ day12/activity1.js
@@ -8,3 +8,19 @@ try {
 } catch (e) {
   console.log(e.message);
 }
+
+//task2: create a function that divides 2 numbers.
+//function must throw error if denominator is 0.
+//handle it using try-catch and log error to console.
+function divideNums(a, b) {
+  if (b === 0) {
+    throw new Error("Cannot divide by zero");
+  } else {
+    return a / b;
+  }
+}
+try {
+  divideNums(3, 0);
+} catch (e) {
+  console.log(e.message);
+}
