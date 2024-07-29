1fd603501a2c00adfc5920f22bd32ce47dd161ce
Prasanna Kamath <kamathp28@gmail.com>
2024-07-29 20:14:56 +0530

    day12 task6 complete

---
+++ day12/activity4.js
@@ -0,0 +1,17 @@
+//task6: create a promise that randomly resolves or rejects.
+//use .catch to handle rejection and log appropriate message to console.
+function promise1() {
+  return new Promise((resolve, reject) => {
+    let rand = Math.round(Math.random() * 100);
+    console.log(rand);
+    if (rand % 2 === 0) {
+      resolve("Promise resolved.");
+    } else {
+      reject("Promise rejected.");
+    }
+  });
+}
+
+promise1()
+  .then((msg) => console.log(msg))
+  .catch((err) => console.log(err));
