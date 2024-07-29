adfa1d0790e2eb3253561d64ea97ea2498eacf80
Prasanna Kamath <kamathp28@gmail.com>
2024-07-29 20:07:44 +0530

    day12 task5 complete

--- day12/activity3.js
+++ day12/activity3.js
@@ -13,3 +13,15 @@ try {
 } catch (err) {
   console.log(err.message);
 }
+
+//task5: Write a function that validates user input and throws custom error if validation fails.
+//handle error using try-catch blocks.
+try {
+  let str1 = "";
+  let str2 = "Literal";
+  if (str1 !== str2) {
+    throw new customError(`String ${str1} not equal to ${str2}`);
+  }
+} catch (err) {
+  console.log(err.message);
+}
