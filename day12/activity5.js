611c9f79721cf9741b0430b0c9f263d938ffec14
Prasanna Kamath <kamathp28@gmail.com>
2024-07-29 20:37:13 +0530

    day12 task9 complete

--- day12/activity5.js
+++ day12/activity5.js
@@ -1,3 +1,14 @@
 //task8: use 'fetch' API to fetch data from invalid url.
 //handle the error using .catch() method.
 fetch("InvalidUrl").catch((err) => console.log(err.message));
+
+//task9: use fetch API to request data from invalid url within async function
+//handle error using try catch block.
+async function handleError() {
+  try {
+    await fetch("INvalidUrl");
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+handleError();
