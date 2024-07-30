2d65ae9e5a8b9d76b60674d6948a57c5ec51f7f4
Prasanna Kamath <kamathp28@gmail.com>
2024-07-30 20:14:07 +0530

    day13 task5 complete

---
+++ day13/activity3.js
@@ -0,0 +1,6 @@
+//task5: create a module that exports multiple constants and functions.
+//import entire module as object and use the properties.
+
+import * as obj from "./module3.js";
+console.log(obj.PI);
+obj.hw();
---
+++ day13/module3.js
@@ -0,0 +1,7 @@
+const PI = 3.14;
+const ZERO = 0;
+function hw() {
+  console.log("Hello world");
+}
+
+export { PI, ZERO, hw };
