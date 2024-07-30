b46eaeceaae94dd22745e929eb02cc6607f032b5
Prasanna Kamath <kamathp28@gmail.com>
2024-07-30 20:42:47 +0530

    day14 task5 complete

---
+++ day14/activity3.js
@@ -0,0 +1,8 @@
+//task5: add a static method to Person class that generates generic greeting message.
+//call this static method without creating instance of class and log the message.
+class Person {
+  static greet() {
+    console.log(`Hello new user, welcome to LPU`);
+  }
+}
+Person.greet();
