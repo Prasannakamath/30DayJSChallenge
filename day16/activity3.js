43d5b29987e723b67b440ba323956b4100d695f4
Prasanna Kamath <kamathp28@gmail.com>
2024-07-31 22:43:06 +0530

    day16 task6 complete

--- day16/activity3.js
+++ day16/activity3.js
@@ -9,3 +9,19 @@ function reverseString(str) {
 console.log(reverseString("prasanna"));
 console.log(reverseString("Eye"));
 console.log(reverseString("String"));
+
+//task6: write recursive function to check if string is a palindrome.
+//log result for few testcases.
+function isPalindrome(str) {
+  if (str.length === 1) {
+    return true;
+  }
+  if (str[0] !== str[str.length - 1]) {
+    return false;
+  }
+  return isPalindrome(str.slice(1, str.length - 1));
+}
+console.log(isPalindrome("prasanna"));
+console.log(isPalindrome("eye"));
+console.log(isPalindrome("Eye"));
+console.log(isPalindrome("RACECAR"));
