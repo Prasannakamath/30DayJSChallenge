e6235102ff4eeb373612f321b9caa14b7cae6f27
Prasanna Kamath <kamathp28@gmail.com>
2024-07-31 22:32:13 +0530

    day16 task4 complete

--- day16/activity2.js
+++ day16/activity2.js
@@ -8,3 +8,15 @@ function sumAll(arr) {
 }
 console.log(sumAll([1, 2, 3, 4, 5]));
 console.log(sumAll([4, 6, 29]));
+
+//task4: write recursive function to find maximum element in an array.
+//log result for few testcases.
+function findMax(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  return Math.max(arr[0], findMax(arr.splice(1, arr.length)));
+}
+console.log(findMax([4, 3, 6, 2, 1, 9]));
+console.log(findMax([3, 3, 3, 3]));
+console.log(findMax([2, 3, 6, 4, 5]));
