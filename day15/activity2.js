52bbdb348a25b62b7f0b64086a1746f8c08ad59b
Prasanna Kamath <kamathp28@gmail.com>
2024-07-31 20:31:25 +0530

    day15 task3 complete

---
+++ day15/activity2.js
@@ -0,0 +1,20 @@
+//task3: Write a function that generates unique Ids.
+//create a closure function that keeps track of last generated Id.
+//increment Id with each call;
+function generateUniqueId() {
+  let idNum = 0;
+  return {
+    generate: function () {
+      return ++idNum;
+    },
+    getLastGenNum: function () {
+      return idNum;
+    },
+  };
+}
+
+let studentId = generateUniqueId();
+
+let id1 = studentId.generate();
+console.log(id1);
+console.log(studentId.getLastGenNum());
