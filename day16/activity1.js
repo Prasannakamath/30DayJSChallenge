22fdfcc559706fea2a5d05158967911b1e7e7222
Prasanna Kamath <kamathp28@gmail.com>
2024-07-31 22:25:04 +0530

    day16 task2 complete

--- day16/activity1.js
+++ day16/activity1.js
@@ -7,3 +7,15 @@ function factorial(num) {
 console.log(factorial(5));
 console.log(factorial(8));
 console.log(factorial(12));
+
+//task2: write recursive function to calculate nth fibonacci number.
+//log result for  few testcases.
+function fibonacci(num) {
+  if (num === 0) return 0;
+  if (num === 1 || num === 2) return 1;
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
+console.log(fibonacci(3));
+console.log(fibonacci(5)); //0 1 1 2 3 5
+console.log(fibonacci(10));
+console.log(fibonacci(15));
