7f850feb0b816eaf8985045678f9d8933d187e1b
Prasanna Kamath <kamathp28@gmail.com>
2024-08-04 22:37:07 +0530

    day18 task5 complete

--- day18/activity2.js
+++ day18/activity2.js
@@ -10,3 +10,20 @@ function linearSearch(arr, data) {
 
 let arr = [2, 3, 4, 5, 6, 10];
 console.log(linearSearch(arr, 10));
+
+//task5: binary search
+function binarySearch(arr, data) {
+  function search(arr, data, low, high) {
+    if (low > high) return -1;
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] === data) {
+      return mid;
+    } else if (arr[mid] < data) {
+      return search(arr, data, mid + 1, high);
+    } else {
+      return search(arr, data, low, mid - 1);
+    }
+  }
+  return search(arr, data, 0, arr.length - 1);
+}
+console.log(binarySearch(arr, 10));
