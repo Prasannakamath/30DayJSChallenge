97ac28d97ee15c0bd7c5ad37e613e61a649a8cc3
Prasanna Kamath <kamathp28@gmail.com>
2024-08-04 20:55:10 +0530

    day18 task2 and task3 complete

--- day18/activity1.js
+++ day18/activity1.js
@@ -16,3 +16,39 @@ function quickSort(arr) {
 
 let arr = [-6, 3, 4, 71, 9, 0];
 console.log(quickSort(arr));
+
+//task2: Selection Sort.
+function SelectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let small = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[small]) {
+        small = j;
+      }
+    }
+    if (small !== i) {
+      let c = arr[small];
+      arr[small] = arr[i];
+      arr[i] = c;
+    }
+  }
+  return arr;
+}
+
+console.log(SelectionSort(arr));
+
+//task3: bubble sort.
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort(arr));
