8ca5be62bd83f270db7982afb43553530555fb30
Prasanna Kamath <kamathp28@gmail.com>
2024-08-05 20:11:20 +0530

    day18 task7 complete

--- day18/activity3.js
+++ day18/activity3.js
@@ -13,3 +13,21 @@ function calcFreq(str) {
 
 let str = "prasanna";
 console.log(calcFreq(str));
+
+//task7: longest substring without repeating characters.
+function longSubStr(str) {
+  let freq = {};
+  let substring = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] in freq) {
+      continue;
+    } else {
+      freq[str[i]] = 1;
+      substring += str[i];
+    }
+  }
+  return substring;
+}
+
+let str1 = "abcabcbb";
+console.log(longSubStr(str1));
