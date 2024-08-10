0ef16999b9acc5070cf8aaf913c676168739d561
Prasanna Kamath <kamathp28@gmail.com>
2024-08-10 20:44:26 +0530

    day19 task10 complete

--- day19/activity5.js
+++ day19/activity5.js
@@ -14,3 +14,17 @@ function validatePassword(password) {
 
 let pass = "Abc123@1";
 console.log(`password: ${pass} is valid? ${validatePassword(pass)}`);
+
+//task10: regex to validate a url.
+function validateUrl(url) {
+  let regex = /^http[s]?:\/\/([\b\w-{2,}]*)[.]*\b(\w{2,}).\b(\w{2,})/;
+  let match = url.match(regex);
+  if (match) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let url = "https://courses.chaicode.com";
+console.log(`is the url: ${url} valid? ${validateUrl(url)}`);
